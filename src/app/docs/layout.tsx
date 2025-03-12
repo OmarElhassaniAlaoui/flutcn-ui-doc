0d48b2805727f986afbe75c6a4ba2562bc696c3f
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-03-12 06:27:39 +0000

    Add dark and light logo SVGs; update layout to use Image component for logo

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -2,7 +2,6 @@ import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
-import { Footer } from 'node_modules/fumadocs-ui/dist/page.client';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 /**
  * Shared layout configurations
@@ -12,15 +13,13 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        Flutcn-ui
+        <Image
+          src="/icons/logo-dark.svg"
+          width={30}
+          height={30}
+          alt="Flutcn-ui Logo"
+        />
+        <span>Flutcn-ui</span>
       </>
     ),
   },
@@ -30,8 +29,5 @@ export const baseOptions: BaseLayoutProps = {
       url: "/docs",
       active: "nested-url",
     },
-  ],  
- 
-
-
+  ],
 };

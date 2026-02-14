8bc4bbc44365cc448d6ecd71ba1c1d01c6948b5d
Omar Elhassani Alaoui <102819564+OmarElhassaniAlaoui@users.noreply.github.com>
2026-02-14 15:01:58 +0100

    bump nextjs v

--- source.config.ts
+++ source.config.ts
@@ -1,34 +1,10 @@
 import {
   defineDocs,
   defineConfig,
-  defineCollections,
-  frontmatterSchema ,
-  metaSchema , 
 } from "fumadocs-mdx/config";
-import { z } from "zod";
 
-export const {docs , meta} = defineDocs({
+export const { docs, meta } = defineDocs({
   dir: "content/docs",
-  docs: {
-    schema: frontmatterSchema.extend({
-      index: z.boolean().default(false),
-    })
-  }, 
-  meta: {
-    schema: metaSchema.extend({
-      description: z.string().optional(),
-    })
-  }
 });
 
-export const widgets = defineCollections({
-  type: "doc",
-  dir: "content/widgets",
-});
-
-
-export default defineConfig({
-  mdxOptions: {
-    // MDX options
-  },
-});
+export default defineConfig({});
--- src/lib/source.ts
+++ src/lib/source.ts
@@ -3,32 +3,9 @@ import { loader } from "fumadocs-core/source";
 import { createMDXSource } from "fumadocs-mdx";
 import { icons } from "lucide-react";
 import { createElement } from "react";
+
 export const source = loader({
   baseUrl: "/docs",
-  source: createMDXSource(docs, meta),
-  icon(icon) {
-    if (!icon) {
-      return;
-    }
-
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
-});
-
-export const widgets = loader({
-  baseUrl: "/docs/widgets",
-  source: createMDXSource(docs, meta),
-  icon(icon) {
-    if (!icon) {
-      return;
-    }
-
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
-});
-
-export const themes = loader({
-  baseUrl: "/docs/themes",
   source: createMDXSource(docs, meta),
   icon(icon) {
     if (!icon) {
